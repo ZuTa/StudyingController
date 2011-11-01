ac4be498ea2fcb0dace2a2e787d5581f4cafbd80
Artem Yermak <snxsnb@gmail.com>
2011-11-02 00:29:16 +0200

    Task 1 *added testing comment

--- StudyingController/StudyingController/App.xaml.cs
+++ StudyingController/StudyingController/App.xaml.cs
@@ -10,7 +10,7 @@ namespace StudyingController
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application //commiting test
     {
     }
 }
