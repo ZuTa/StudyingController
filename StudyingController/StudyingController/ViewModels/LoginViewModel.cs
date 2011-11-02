5f97ed5bf21f5c067e27f379523a5d8fdbd0a42e
Artem Yermak <snxsnb@gmail.com>
2011-11-02 17:50:58 +0200

    Task 25 *added RelayCommand and LoginConfig classes

--- StudyingController/StudyingController/ViewModels/LoginViewModel.cs
+++ StudyingController/StudyingController/ViewModels/LoginViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using StudyingController.Common;
 using System.Windows.Threading;
+using System.Windows.Input;
 
 namespace StudyingController.ViewModels
 {
@@ -28,19 +29,36 @@ namespace StudyingController.ViewModels
 
         #region Commands
 
+        private RelayCommand loginCommand;
+        public ICommand LoginCommand
+        {
+            get 
+            {
+                if (loginCommand == null)
+                    loginCommand = new RelayCommand((param) => UserLogin(), (param) => CanUserLogin()); 
 
+                return loginCommand;
+            }
+        }
 
         #endregion
 
         #region Methods
 
+        private void UserLogin()
+        {
+ 
+        }
 
-
+        private bool CanUserLogin()
+        {
+            return true;
+        }
         #endregion
 
         #region Events
 
-
+        public event EventHandler LoginEvent;
 
         #endregion
     }
