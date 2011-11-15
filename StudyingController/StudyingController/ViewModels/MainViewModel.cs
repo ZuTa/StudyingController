d8567f57321b744db54c5870639b0811f3cf9d95
snx <snx@snxPC.(none)>
2011-11-16 00:24:52 +0200

    Task 33,34

--- StudyingController/StudyingController/ViewModels/MainViewModel.cs
+++ StudyingController/StudyingController/ViewModels/MainViewModel.cs
@@ -4,14 +4,62 @@ using System.Linq;
 using System.Text;
 using StudyingController.Common;
 using System.Windows.Threading;
+using System.Windows.Input;
 
 namespace StudyingController.ViewModels
-{
+{   
     public class MainViewModel : BaseApplicationViewModel
     {
+        #region Fields & Properties
+
+        private BaseApplicationViewModel currentWorkspace = null; 
+
+        public BaseApplicationViewModel CurrentWorkspace
+        {
+            get 
+            { 
+                return currentWorkspace; 
+            }
+            set 
+            {
+                if (currentWorkspace != value)
+                    currentWorkspace = value;
+                OnPropertyChanged("CurrentWorkspace");
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        
         public MainViewModel(IUserInterop userInterop, IControllerInterop controllerInterop, Dispatcher dispatcher)
             : base(userInterop, controllerInterop, dispatcher)
         {
         }
+
+        #endregion
+
+        #region Commands
+
+        private RelayCommand universityStructureCommand;
+        public ICommand UniversityStructureCommand
+        {
+            get 
+            {
+                if (universityStructureCommand == null) universityStructureCommand = new RelayCommand((param) => OpenUniversityStructure());
+                return universityStructureCommand;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OpenUniversityStructure()
+        {
+            CurrentWorkspace = new UniversityStructureViewModel(UserInterop, ControllerInterop, Dispatcher);
+        }
+
+        #endregion
     }
 }
