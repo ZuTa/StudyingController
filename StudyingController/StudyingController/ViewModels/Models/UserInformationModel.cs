d9b4ed4e20aa640dde3b230147e61c9329a37bc7
snx <snx@snxPC.(none)>
2012-02-02 18:15:34 +0200

    Task 39 *fixed bug
    Additionally
    *fixed e-mail validation bug

--- StudyingController/StudyingController/ViewModels/Models/UserInformationModel.cs
+++ StudyingController/StudyingController/ViewModels/Models/UserInformationModel.cs
@@ -116,7 +116,7 @@ namespace StudyingController.ViewModels.Models
                 error = Properties.Resources.ErrorFieldEmpty;
                 return false;
             }
-            if (!Regex.IsMatch(email, "^[a-z\\-\\.]+@[a-z\\-]+\\.[a-z]{2,3}$"))
+            if (!Regex.IsMatch(email, @"^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$"))
             {
                 error = Properties.Resources.ErrorBadCharsUsed;
                 return false;
