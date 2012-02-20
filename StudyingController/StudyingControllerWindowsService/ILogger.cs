c5c644736776c5119d36ddd59ca9a9ed15bdd3ab
ZuTa <taras.zubyk@i.ua>
2012-02-21 00:19:52 +0200

    * added ILogger * added Modeling Project

---
+++ StudyingController/StudyingControllerWindowsService/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace StudyingControllerWindowsService
+{
+
+    public interface ILogger
+    {
+
+        void Write(string message, EventLogEntryType messageType);
+
+    }
+}
