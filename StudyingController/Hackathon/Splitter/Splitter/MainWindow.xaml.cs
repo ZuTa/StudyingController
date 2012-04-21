7b7d2eaa452dcc5e547aeeac1d45e054d1e66142
maksimPizhov <maksim.pizhov@gmail.com>
2012-04-21 21:21:01 +0300

    *commit

--- StudyingController/Hackathon/Splitter/Splitter/MainWindow.xaml.cs
+++ StudyingController/Hackathon/Splitter/Splitter/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace Splitter
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : RibbonWindow
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
