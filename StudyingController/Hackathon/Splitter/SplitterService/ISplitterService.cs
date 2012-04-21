e82a141165c0d6200d749fe551675bc47eab55bd
ZuTa <taras.zubyk@i.ua>
2012-04-21 22:57:02 +0300

    * added CanRegister and Register methods to service

--- StudyingController/Hackathon/Splitter/SplitterService/ISplitterService.cs
+++ StudyingController/Hackathon/Splitter/SplitterService/ISplitterService.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using ModelDTO;
 
 namespace SplitterService
 {
@@ -14,6 +15,14 @@ namespace SplitterService
         [OperationContract]
         [FaultContract(typeof(SplitterServiceException))]
         Session Login(string login, string password);
+
+        [OperationContract]
+        [FaultContract(typeof(SplitterServiceException))]
+        bool CanRegister(SystemUserDTO user);
+
+        [OperationContract]
+        [FaultContract(typeof(SplitterServiceException))]
+        void Register(SystemUserDTO user);
 
     }
 }
