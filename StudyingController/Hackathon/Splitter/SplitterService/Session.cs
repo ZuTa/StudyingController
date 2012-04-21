15edf7ab153405733dd3f587d20a2f9bf71c26e4
ZuTa <taras.zubyk@i.ua>
2012-04-21 20:37:20 +0300

    *added wcf service

---
+++ StudyingController/Hackathon/Splitter/SplitterService/Session.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace SplitterService
+{
+    [DataContract]
+    public class Session
+    {
+        #region Fields & Properties
+
+        private static Random random = new Random();
+        private static int counter = 0;
+
+        private SystemUserDTO user;
+        [DataMember]
+        public SystemUserDTO User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+
+        public DateTime LastAccessTime { get; set; }
+
+        [DataMember]
+        public double SessionID { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Session(SystemUserDTO user, StudyRangeDTO studyRange)
+        {
+            this.user = user;
+
+            SessionID = random.NextDouble();
+            SessionID += System.Threading.Interlocked.Increment(ref counter);
+            LastAccessTime = DateTime.UtcNow;
+        }
+
+        #endregion
+    }
+}
